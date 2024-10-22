01cf79673761e641c98df73ac05dc0b186ad4af0
linfengfrank <linfengfrank@gmail.com>
2024-10-22 19:55:52 +0800

    Update the python code of Question 2 of Tutorial 9

--- Tutorial_9/Tut9_Q2.py
+++ Tutorial_9/Tut9_Q2.py
@@ -1,27 +1,39 @@
-import numpy as np
-from sklearn.metrics import mean_squared_error
+#!/usr/bin/env python3
 
-# Given data points
-data = {
-    1: 2, 0.8: 3, 2: 2.5, 2.5: 1, 3: 2.3, 4: 2.8, 4.2: 1.5, 
-    6: 2.6, 6.3: 3.5, 7: 4, 8: 3.5, 8.2: 5, 9: 4.5
-}
+import numpy as np
+
+def mse(y):
+    return np.mean(np.power(y-np.mean(y),2))
+
+
+# Given data
+data = np.array([
+    [1, 2], [0.8, 3], [2, 2.5], [2.5, 1], [3, 2.3], [4, 2.8], 
+    [4.2, 1.5], [6, 2.6], [6.3, 3.5], [7, 4], [8, 3.5], [8.2, 5], [9, 4.5]
+])
+
+# Split data into x and y
+x = data[:, 0]
+y = data[:, 1]
 
 # Decision threshold
 threshold = 5.0
 
 # Split the data into two groups based on the decision threshold
-group1 = {x: y for x, y in data.items() if x <= threshold}
-group2 = {x: y for x, y in data.items() if x > threshold}
+ind_left  = np.where( x<=threshold )  # indies of x values less than or equal to the threshold
+ind_right = np.where( x> threshold )  # indies of x values greater than the threshold
 
-# Calculate the MSE for each group
-mse_group1 = mean_squared_error(list(group1.values()), [np.mean(list(group1.values()))] * len(group1))
-mse_group2 = mean_squared_error(list(group2.values()), [np.mean(list(group2.values()))] * len(group2))
+y_left  =  y[ind_left]
+y_right = y[ind_right]
 
-# Compute the overall MSE by averaging the MSEs of the two groups
-overall_mse = (mse_group1 + mse_group2) / 2
+j_1 = len(y_left)
+j_2 = len(y_right)
+N   = len(y)
 
-overall_mse_root = mean_squared_error(list(data.values()), [np.mean(list(data.values()))] * len(data))
+# Compute the MSE for difference levels
+level_1_mse = mse(y)
 
-print(f"Overall MSE at Root: {overall_mse_root}")
-print(f"Overall MSE at Depth 1: {overall_mse}")
+level_2_mse = (j_1/N)*mse(y_left) + (j_2/N)*mse(y_right)
+
+print(f"The level 1 MSE is: {round(level_1_mse, 4)}")
+print(f"The level 2 MSe is: {round(level_2_mse, 4)}")
